6a914d2857480bab89a433113e1ecd7e9f246d9b
oleg086rus <o.pototskiy@yandex.ru>
2022-07-12 15:29:34 +0500

    .

---
+++ index.js
@@ -0,0 +1,38 @@
+const yargs = require('yargs')
+const pkg = require('./package.json')
+const {addNote, printNotes, removeNote} = require('./notes.controller')
+
+yargs.version(pkg.version)
+
+yargs.command({
+  command: 'add',
+  describe: 'Add new note to list',
+  builder: {
+    title: {
+      type: 'string',
+      describe: 'Node title',
+      demandOption: true
+    }
+  },
+  handler({title}) {
+    addNote(title)
+  }
+})
+yargs.command([
+  {
+    command: 'list',
+    describe: 'Print all note',
+    async handler() {
+      printNotes()
+    }
+  },
+  {
+    command: 'delete',
+    describe: 'Remove note',
+    async handler() {
+      removeNote()
+    }
+  }
+])
+
+yargs.parse()
