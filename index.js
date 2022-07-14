2a56f7b550b359ab37a3b2e97cd51c2b754544c0
oleg086rus <o.pototskiy@yandex.ru>
2022-07-14 15:28:50 +0500

    express

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
-const http = require('http')
-
 const chalk = require('chalk')
+const path = require('path')
+const {addNote} = require('./notes.controller')
+const express = require('express')
 
 const port = 3000
+const basePath = path.join(__dirname, 'pages')
+const app = express()
 
-const server = http.createServer((req, res) => {
-  console.log('Server!')
-  
-  res.end('Hello from server!')
+app.use(express.urlencoded({
+  extended: true
+}))
+
+app.get('/', (req, res) => {
+  res.sendfile(path.join(basePath, 'index.html'))
 })
 
-server.listen(port, () => {
+app.post('/', async (req, res) => {
+  await addNote(req.body.title)
+  res.sendFile(path.join(basePath, 'index.html'))
+})
+
+app.listen(port, () => {
   console.log(chalk.green(`Server has been started on port ${port} ...`))
 })
